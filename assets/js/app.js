7eeafdd79bf2fa83f232e8f95b5c6fa136a67ce8
Luz Gutierrez <mili01gm@gmail.com>
2017-03-23 14:56:10 -0500

    Correción

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,14 +1,41 @@
-function Datos() {
-    var nombre = document.getElementById("name");
-    var apellido = document.getElementById("lastname");
-    var dni = document.getElementById("id");
-    var adress = document.getElementById("adress");
-    var info = document.getElementById("info")
+function Mostrar(name,lastname,id,home){
+    this.name = name;
+    this.lastname = lastname;
+    this.id = id;
+    this.home = home;
 
-    info.innerHTML =
-      "<ul>" + "<li>" + "Nombre: " + nombre.value + "</li>" +
-               "<li>" + "Apellido: " + apellido.value + "</li>" +
-               "<li>" + "DNI: " + dni.value + "</li>" +
-               "<li>" + "Dirección: " + adress.value + "</li>" + "</ul>"
-
+    this.muestra = function(){
+      return "<ul>"+"<li>"+"Nombre: " + this.name + "</li>"+
+             "<li>"+"Apellido: " + this.lastname + "</li>" +
+             "<li>"+"DNI: " + this.id + "</li>" +
+             "<li>"+"Dirección: " + this.home + "</li>" + "</ul>"
+    };
 }
+
+window.addEventListener('load', function(){
+  var enviar = document.getElementById("enviar");
+  enviar.addEventListener('click',function(e){
+    e.preventDefault();
+    var nombre = document.getElementById("name").value;
+    var apellido = document.getElementById("lastname").value;
+    var dni = document.getElementById("id").value;
+    var adress = document.getElementById("adress").value;
+    var pattern = /([A-Z]{1}[a-zá-ú]{1,}\s?)/;
+    var info = document.getElementById('info');
+
+    var datos = new Mostrar (nombre,apellido,dni,adress);
+
+    if(nombre == "" || apellido == "" || dni < 8 || adress == ""){
+      alert("Debe completar todos los campos");}
+    else if(!pattern.test(nombre)){
+      alert("Ingrese un nombre válido con la primera letra mayúscula");}
+    else if(!pattern.test(apellido)){
+      alert("Ingrese un apellido válido con la primera letra mayúscula");}
+    else if(!pattern.test(adress)){
+      alert("Ingrese una dirección con la primera letra en mayúscula");}
+    else{ info.innerHTML = datos.muestra();}
+
+  });
+
+
+});
